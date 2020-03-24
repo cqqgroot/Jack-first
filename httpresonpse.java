42bca5854a67cde50deb31cfd86107d11f9c0275
jack-java <xiaonei4151@126.com>
2020-03-24 15:07:34 +0800

    chaoji

---
+++ httpresonpse.java
@@ -0,0 +1,64 @@
+package com.imooc.spring;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.ServerHttpResponse;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class httpresonpse implements ServerHttpResponse {
+    public httpresonpse() {
+        super();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    @Override
+    public void setStatusCode(HttpStatus httpStatus) {
+
+    }
+
+    @Override
+    public void flush() throws IOException {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public OutputStream getBody() throws IOException {
+        return null;
+    }
+
+    @Override
+    public HttpHeaders getHeaders() {
+        return null;
+    }
+}
